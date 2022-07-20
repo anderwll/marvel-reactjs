7a0f4d049e808cca0eb3726bb5b1534b531f61a1
Anderson Wilmsen <ander.wilmsen98@gmail.com>
2022-07-20 11:31:17 -0300

    Corrigido chaves de acesso a api

--- src/services/marvel/config.ts
+++ src/services/marvel/config.ts
@@ -1,4 +1,4 @@
-const privateKey = 'b8bf1aeee5d5e921c4f53d2bb4674e5fd7e53ce3'
-const publicKey = '4decd1a4d8a685e0314853a1583687ec'
+const privateKey = process.env.REACT_APP_MARVEL_PRIVATE_KEY
+const publicKey = process.env.REACT_APP_MARVEL_PUBLIC_KEY
 
 export { privateKey, publicKey }
